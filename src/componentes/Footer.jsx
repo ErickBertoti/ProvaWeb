7d4d526b8b7d096b00adf2b0231421fbe5076b4e
ErickBertoti <erickhoffmann2301@gmail.com>
2024-06-26 21:29:34 -0300

    First commit

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 MyFinance. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
